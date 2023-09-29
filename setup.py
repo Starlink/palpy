f44f64397081941f4eba71a018888190021ace4d
Tim Jenness <tim.jenness@gmail.com>
2023-09-29 12:44:04 -0700

    Remove python 2.7 reference (no longer checked)

--- setup.py
+++ setup.py
@@ -180,7 +180,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Cython',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: C',
         'Programming Language :: Python :: Implementation :: CPython',
